33ee9f0b1c6ccaad42a086154ed1c3f8a6010142
Emanuel Vargas <emanuel.vargas@utp.ac.pa>
2026-08-29 12:59:30 -0500

    Repositorio creado

---
+++ LibrodeCalificaciones/LibroCalificacion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibrodeCalificaciones
+{
+    public class LibroCalificacion
+    {
+        public void MostrarMensaje()
+        {
+            Console.WriteLine("Bienvenido al libro de calificaciones");
+        }
+    }
+}
---
+++ LibrodeCalificaciones/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LibrodeCalificaciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LibroCalificacion MiLibro = new LibroCalificacion();
+            MiLibro.MostrarMensaje();
+        }
+    }
+}
